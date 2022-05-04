ab6dd440bc70c942aa7823f3d9ce9f4e9f341eae
Walid Yachou <walid.yachou@capgemini.com>
2022-05-04 16:31:00 +0200

    feat: implement search

--- components/molecules/Commits/Commits.tsx
+++ components/molecules/Commits/Commits.tsx
@@ -1,15 +1,36 @@
-import { CommitItem } from "components/atoms";
+import { ChangeEvent, useState } from "react";
+import { CommitItem, Spacer, TextField } from "components/atoms";
 import { CommitsProperties } from "./Commits.properties";
 import { NOT_FOUND_MESSAGE } from "constants/common";
+import { Size } from "components/atoms/Spacer/Size";
 
 export const Commits: React.FC<CommitsProperties> = ({ commits }) => {
+  const [search, setSearch] = useState("");
+  const { message: serverMessage } = commits;
+
   return (
-    <div className="flex flex-col">
-      {commits.message === NOT_FOUND_MESSAGE && <div>Not found...</div>}
-      {commits.message !== NOT_FOUND_MESSAGE &&
-        commits.map((commit: any) => (
-          <CommitItem key={commit.sha} commitData={commit} />
-        ))}
-    </div>
+    <>
+      <TextField
+        value={search}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setSearch(e.target.value)
+        }
+        className="w-full lg:w-80 lg:mr-2"
+        placeholder="Search commit"
+      />
+      <Spacer size={Size.EXTRA_SMALL} />
+
+      <div className="flex flex-col">
+        {serverMessage === NOT_FOUND_MESSAGE && <div>Not found...</div>}
+        {serverMessage !== NOT_FOUND_MESSAGE &&
+          commits
+            .filter((data: any) =>
+              data.commit.message.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((commit: any) => (
+              <CommitItem key={commit.sha} commitData={commit} />
+            ))}
+      </div>
+    </>
   );
 };
